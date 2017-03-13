edda07b135a833d8e5798988054fb2d8c44c2f4f
Elliot Lo <elliotlo@gmail.com>
2017-03-13 18:29:49 +0800

    First Commit

---
+++ Source/SlateLayoutDemo/SlateLayoutDemoGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SlateLayoutDemo.h"
+#include "SlateLayoutDemoGameModeBase.h"
+
+
+
+
---
+++ Source/SlateLayoutDemo/SlateLayoutDemoGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SlateLayoutDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLATELAYOUTDEMO_API ASlateLayoutDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
